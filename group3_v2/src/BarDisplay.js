4470e09f2ad30886e103dd3f9b035f19a25c514a
elainadecicco <56656822+elainadecicco@users.noreply.github.com>
2019-10-31 12:12:24 -0400

    Add files via upload

---
+++ group3_v2/src/BarDisplay.js
@@ -0,0 +1,33 @@
+import * as d3 from "d3";
+
+
+
+export default class BarDisplay {
+    constructor(barData, barHolder, barWidth, barHeight,padding) {
+        this.w = 1400;
+        this.h = 1400;
+        this.padding = padding;
+        this.barHolder = barHolder;
+        //this.dataset = [50, 10, 14, 20, 25];
+        this.stats = barData.myData;
+        this.buildChart();
+    }
+
+    buildChart() {
+        let svg = d3.select(this.barHolder)
+            .attr("width", this.w)
+            .attr("height", this.h);
+
+        svg.selectAll("rect")
+            .data(this.stats)
+            .enter()
+            .append("rect")
+            .attr("x", (d, i) => i * (this.w / this.stats.length))
+            .attr("y", d => this.h - (d))
+            .attr("width", this.w / this.stats.length - this.padding)
+            .attr("height", d => d)
+            .attr("fill", d => `rgb(${d*10},0,0)`);
+    }
+
+    
+}
