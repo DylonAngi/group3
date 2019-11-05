33787ddce239650bcc43e258e04efa5c67362fb2
Dylon Angi <38168292+DylonAngi@users.noreply.github.com>
2019-11-05 13:52:14 -0500

    Add files via upload

---
+++ bar2.js
@@ -0,0 +1,64 @@
+document.addEventListener('DOMContentLoaded', () => {
+  var render = (selector, size, data) => {
+    var margin = size.margin;
+    var width = size.width - margin.left - margin.right;
+    var height = size.height - margin.top - margin.bottom;
+
+    var x = d3.scaleLinear()
+      .domain(d3.extent(data, d => d.temp))
+      .range([0, width]);
+
+    var y = d3.scaleBand()
+      .domain(data.map(d => d.year))
+      .rangeRound([0, height])
+      .padding(0.2);
+
+    var xAxis = d3.axisBottom(x);
+
+    var yAxis = d3.axisLeft(y)
+      .tickSize(0);
+
+    var svg = d3.select(selector)
+      .attr('width', size.width)
+      .attr('height', size.height);
+
+    var chart = svg.append('g')
+      .attr('transform', `translate(${ margin.left }, ${ margin.top })`);
+
+    var bar = chart.selectAll('.bar')
+      .data(data)
+      .enter().append('rect')
+        .attr('class', d => `bar ${ d.temp < 0 ? 'negative': 'positive' }`)
+        .attr('x', d => x(Math.min(0, d.temp)))
+        .attr('y', d => y(d.year))
+        .attr('width', d => Math.abs(x(d.temp) - x(0)))
+        .attr('height', y.bandwidth());
+
+    chart.append('g')
+      .attr('transform', `translate(0, ${ height })`)
+      .attr('class', 'axis x')
+      .call(xAxis);
+
+    chart.append('g')
+      .attr('class', 'axis y')
+      .attr('transform', `translate(${ x(0) }, 0)`)
+      .call(yAxis);
+  };
+
+  fetch('data.json')
+    .then(data => data.json())
+    .then(data => {
+      var settings = {
+        width: 1000,
+        height: 600,
+        margin: {
+          top: 20,
+          right: 20,
+          bottom: 30,
+          left: 40
+        }
+      };
+
+      render('#chart2', settings, data);
+    });
+});
